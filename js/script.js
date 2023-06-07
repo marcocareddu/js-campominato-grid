eaf255fa398e92cdc2031c751658fa54bc866e90
marcocareddu <saldumaccu@tuta.io>
2023-06-07 15:44:20 +0200

    JS - Added Dataset to Function

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ console.log('JS OK')
 function createCell(numero) {
     let element = '';
     for (let i = 1; i <= numero; i++) {
-        element += `<div class="cell-${numero}">${i}</div>`;
+        element += `<div class="cell cell-${numero}" data-="${i}">${i}</div>`;
     }
     return element;
 }
