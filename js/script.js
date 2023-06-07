f605cf1c0b3efd0c812349105e63a5fa080ba226
marcocareddu <saldumaccu@tuta.io>
2023-06-07 15:23:06 +0200

    JS - Added createCell function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 console.log('JS OK')
+
+// *PHASE 1
+
+//? *********************** Functions ***********************
+
+// Create Function for cells
+function createCell(numero) {
+    let element = '';
+    for (let i = 1; i <= numero; i++) {
+        element += `<div class="cell-${numero}">${i}</div>`;
+    }
+    return element;
+}
+
+//? *********************************************************
+
+
+// Link DOM elements
+
+// Create Variables
+
+
+
+
