fccf737bff3f5e73e787267e95f255bf65afcfa8
marcocareddu <saldumaccu@tuta.io>
2023-06-07 15:33:12 +0200

    JS - Inject strings in DOM

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ console.log('JS OK')
 // *PHASE 1
 
 //? *********************** Functions ***********************
-
 // Create Function for cells
 function createCell(numero) {
     let element = '';
@@ -12,18 +11,24 @@ function createCell(numero) {
     }
     return element;
 }
-
 //? *********************************************************
+
+
 
 // Link DOM elements
 const grid = document.querySelector('.grid');
 const selectElement = document.getElementById('difficulty');
 const button = document.querySelector('button');
 
+
+// * PHASE 2
 // Button click - Create Variables 
 button.addEventListener('click', function () {
     const difficulty = parseInt(selectElement.value);
     console.log(difficulty);
+
+    // Inject string in DOM
+    grid.innerHTML = createCell(difficulty);
 }
 
 )
